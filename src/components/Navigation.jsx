1a2deaaee8bb9232fb93e22de1b31bec17671508
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-02-08 00:56:22 +0100

    Separate Logout logo
    Go on #9

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import LogoutLogo from './svg/Logout.jsx'
+
 export function Navigation() {
    return (
       <nav className='bg-secundario flex justify-between px-5 py-3'>
@@ -10,21 +12,7 @@ export function Navigation() {
          </ol>
 
          <a href='/login' className='hover-resize'>
-            <svg
-               width='25'
-               height='25'
-               viewBox='0 0 48 48'
-               fill='none'
-               xmlns='http://www.w3.org/2000/svg'
-            >
-               <path
-                  d='M18 42H10C8.93913 42 7.92172 41.5786 7.17157 40.8284C6.42143 40.0783 6 39.0609 6 38V10C6 8.93913 6.42143 7.92172 7.17157 7.17157C7.92172 6.42143 8.93913 6 10 6H18M32 34L42 24M42 24L32 14M42 24H18'
-                  stroke='#F5F5F5'
-                  strokeWidth='4'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-               />
-            </svg>
+            <LogoutLogo />
          </a>
       </nav>
    )
