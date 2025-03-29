fc928fab809fb1d574bcef2afe72bbf9deec6593
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-03-29 11:33:12 +0100

    Change the order of sections
    Order alphabetically. Go on #30

--- src/pages/admin/Principal.jsx
+++ src/pages/admin/Principal.jsx
@@ -14,14 +14,14 @@ export function AdminPage() {
          </header>
          <div className='mygrid m-5 grow content-center justify-center gap-8'>
             <ImageLink
-               title='Usuarios'
-               href={Routes.AdminUsuarios}
-               imgSrc={logo_users}
-            />
-            <ImageLink
                title='Registros'
                href={Routes.AdminRegistros}
                imgSrc={logo_registros}
+            />
+            <ImageLink
+               title='Usuarios'
+               href={Routes.AdminUsuarios}
+               imgSrc={logo_users}
             />
          </div>
       </AppNavFrame>
