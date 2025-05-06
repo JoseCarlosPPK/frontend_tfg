e383a0a87ca0b0b8462f2faca55e4e0da2584d32
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-05-06 13:00:58 +0200

    Changing the Submit Button
    If this component is included within a form, if it's the only button, it will be the submit button. If another submit button is used, it will do what the onClick function tells it to. Closes #37

--- src/components/inputs/Search.jsx
+++ src/components/inputs/Search.jsx
@@ -1,19 +1,29 @@
+import PropTypes from 'prop-types'
 import { Input } from '.'
 import { SearchLogo } from '../svg'
 
-export function Search({ ...props }) {
+export function Search({ className, onClick = null, ...props }) {
    return (
       <div className='relative'>
          <Input
             type='search'
             name='search'
             placeholder='Usuario'
-            className='p-2.5 pr-10'
+            className={`${className} p-2.5 pr-10`}
             {...props}
          />
-         <button type='submit' className='absolute right-1 top-2 text-gray-600'>
+         <button
+            type='button'
+            className='absolute right-1 top-2 text-gray-600'
+            onClick={onClick}
+         >
             <SearchLogo />
          </button>
       </div>
    )
+}
+
+Search.propTypes = {
+   className: PropTypes.string,
+   onClick: PropTypes.func,
 }
