eb1328063c778f1713a26805f39b6c2de9c5b539
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-05-07 20:26:30 +0200

    Add POST /farmacias
    Go on #34

--- src/services/request.js
+++ src/services/request.js
@@ -67,6 +67,10 @@ export class Request {
       return Request.request(`${this.#endpoints.farmacias}/${id}`, 'DELETE')
    }
 
+   addFarmacia(farmacia) {
+      return Request.request(this.#endpoints.farmacias, 'POST', farmacia)
+   }
+
    getPersonas(name, page, perPage) {
       return Request.request(
          `${this.#endpoints.personas}?page=${page}&per_page=${perPage}&name=${name}`,
