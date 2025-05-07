c3021cd668db7719f028e3a9c6eb5ac0e55575d5
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-05-07 22:58:04 +0200

    Add functionality to ADD farmacias.
    With new tutors and existing tutors. Go on #36

--- src/pages/Centros.jsx
+++ src/pages/Centros.jsx
@@ -5,6 +5,7 @@ import { AppNavFrame } from '../components/AppFrame.jsx'
 import { Breadcrumb } from '../components/Breadcrumb.jsx'
 import {
    AddButton,
+   ArrowButton,
    Button,
    DeleteButton,
    EditButton,
@@ -14,6 +15,7 @@ import { Modal } from '../components/Modal.jsx'
 import { FarmaciasColumnas } from '../components/models/columns.jsx'
 import { filters } from '../components/models/filters.js'
 import { AUTO_HIDE_DURATION } from '../components/snacbarks'
+import { Direccion } from '../components/svg/arrow_direction.js'
 import { Table } from '../components/Table.jsx'
 import { useAuth, useQueryString } from '../hooks'
 import { request } from '../services'
@@ -33,6 +35,11 @@ export function CentrosPage() {
    const [openAdd, setOpenAdd] = useState(false)
    const [tutores, setTutores] = useState([])
    const notifications = useNotifications()
+   const [tutoresBuscados, setTutoresBuscados] = useState([])
+   const [pageTutores, setPageTutores] = useState(1)
+   const [totalTutores, setTotalTutores] = useState(0)
+   const [searchTutores, setSearchTutores] = useState('')
+   const perPageTutores = 10
 
    const FarmaciasColumnasEdited = [
       ...FarmaciasColumnas,
@@ -59,8 +66,44 @@ export function CentrosPage() {
    /**
     * Añade un nuevo input para añadir un tutor/a
     */
-   function handleAddTutor() {
-      setTutores((prev) => [...prev, { idTmp: crypto.randomUUID() }])
+   function handleAddTutor(persona = {}) {
+      setTutores((prev) => [
+         ...prev,
+         Object.assign(persona, { idTmp: crypto.randomUUID() }),
+      ])
+   }
+
+   function existeTutor(buscado) {
+      return tutores.some((tutor) => tutor.id === buscado.id)
+   }
+
+   function fetchTutores(name, page, perPage) {
+      request
+         .getPersonas(name, page, perPage)
+         .then((res) => {
+            if (res.ok) {
+               res.json().then((resJson) => {
+                  setTutoresBuscados(resJson.data)
+                  setTotalTutores(resJson.total)
+               })
+            } else {
+               if (res.status === 401) {
+                  signOut()
+               } else {
+                  notifications.show('Algo falló', {
+                     severity: 'error',
+                     autoHideDuration: AUTO_HIDE_DURATION,
+                  })
+               }
+            }
+         })
+         .catch(() => {
+            // alerta de error
+            notifications.show('Fallo de conexión', {
+               severity: 'error',
+               autoHideDuration: AUTO_HIDE_DURATION,
+            })
+         })
    }
 
    function fetchData() {
@@ -78,7 +121,9 @@ export function CentrosPage() {
                   setTotalCentros(resJson.total)
                })
             } else {
-               signOut()
+               if (res.status === 401) {
+                  signOut()
+               }
             }
          })
          .catch(() => {
@@ -169,7 +214,73 @@ export function CentrosPage() {
                setOpenAdd(false)
             }}
             onConfirm={(event) => {
-               event.prevenDefault()
+               event.preventDefault()
+               const tutoresFormateados = tutores.map((value) => {
+                  if (value.id) {
+                     return {
+                        id: value.id,
+                        nombre: value.nombre,
+                     }
+                  } else {
+                     return {
+                        nombre: value.nombre,
+                     }
+                  }
+               })
+
+               const formData = new FormData(event.target)
+               const centroACrear = {
+                  personas: tutoresFormateados,
+                  nombre: formData.get('nombre'),
+                  direccion: formData.get('direccion'),
+                  localidad: formData.get('localidad'),
+                  provincia: formData.get('provincia'),
+                  cp: formData.get('cp'),
+                  correo: formData.get('correo'),
+                  telefono: formData.get('telefono'),
+                  movil: formData.get('movil'),
+               }
+
+               request
+                  .addFarmacia(centroACrear)
+                  .then((res) => {
+                     if (res.ok) {
+                        // limpieza de estados
+                        setOpenAdd(false)
+                        setTutores([])
+                        setTutoresBuscados([])
+                        setPageTutores(1)
+                        setSearchTutores('')
+
+                        // alerta de éxito
+                        notifications.show(
+                           `El centro ${centroACrear.nombre} ha sido creado`,
+                           {
+                              severity: 'success',
+                              autoHideDuration: AUTO_HIDE_DURATION,
+                           }
+                        )
+                     } else {
+                        if (res.status === 401) {
+                           signOut()
+                        }
+                        // alerta de error
+                        notifications.show(
+                           `El centro ${centroACrear.nombre} NO ha sido creado`,
+                           {
+                              severity: 'error',
+                              autoHideDuration: AUTO_HIDE_DURATION,
+                           }
+                        )
+                     }
+                  })
+                  .catch(() => {
+                     // alerta de error
+                     notifications.show('Fallo de conexión', {
+                        severity: 'error',
+                        autoHideDuration: AUTO_HIDE_DURATION,
+                     })
+                  })
             }}
             title='Añadir un nuevo centro'
             form={true}
@@ -193,6 +304,16 @@ export function CentrosPage() {
                         Buscar al tutor/a
                      </label>
                      <Search
+                        value={searchTutores}
+                        onChange={(event) => {
+                           const newValue = event.target.value
+                           setSearchTutores(newValue)
+                           setPageTutores(1)
+
+                           if (newValue === '') {
+                              setTutoresBuscados([])
+                           }
+                        }}
                         name='tutorBuscado'
                         id='tutorBuscado'
                         placeholder='Tutor/a'
@@ -200,9 +321,88 @@ export function CentrosPage() {
                         onKeyDown={(event) => {
                            if (event.key === 'Enter') {
                               event.preventDefault()
+                              fetchTutores(
+                                 searchTutores,
+                                 pageTutores,
+                                 perPageTutores
+                              )
                            }
                         }}
+                        onClick={() => {
+                           fetchTutores(
+                              searchTutores,
+                              pageTutores,
+                              perPageTutores
+                           )
+                        }}
                      />
+
+                     {tutoresBuscados.length > 0 && (
+                        <div className='divide-y divide-gray-400'>
+                           {tutoresBuscados.map((value) => {
+                              return (
+                                 <p
+                                    className='hover-resalt flex items-center justify-between rounded-lg p-1 hover:cursor-pointer'
+                                    key={value.id}
+                                    onClick={() => {
+                                       if (!existeTutor(value)) {
+                                          handleAddTutor(value)
+                                       }
+                                    }}
+                                 >
+                                    {value.nombre}
+                                    {existeTutor(value) && (
+                                       <span className='rounded-lg bg-green-700 p-1 text-white'>
+                                          seleccionado
+                                       </span>
+                                    )}
+                                 </p>
+                              )
+                           })}
+                           <div className='mx-2 flex items-center justify-between'>
+                              <ArrowButton
+                                 type='button'
+                                 size='size-8'
+                                 direccion={Direccion.left}
+                                 disabled={pageTutores === 1}
+                                 className={
+                                    pageTutores === 1 ? 'text-gray-600' : ''
+                                 }
+                                 onClick={() => {
+                                    const newPage = pageTutores - 1
+                                    setPageTutores(newPage)
+                                    fetchTutores(
+                                       searchTutores,
+                                       newPage,
+                                       perPageTutores
+                                    )
+                                 }}
+                              />
+                              <ArrowButton
+                                 type='button'
+                                 size='size-8'
+                                 direccion={Direccion.right}
+                                 disabled={
+                                    pageTutores * perPageTutores >= totalTutores
+                                 }
+                                 className={
+                                    pageTutores * perPageTutores >= totalTutores
+                                       ? 'text-gray-600'
+                                       : ''
+                                 }
+                                 onClick={() => {
+                                    const newPage = pageTutores + 1
+                                    setPageTutores(newPage)
+                                    fetchTutores(
+                                       searchTutores,
+                                       newPage,
+                                       perPageTutores
+                                    )
+                                 }}
+                              />
+                           </div>
+                        </div>
+                     )}
                   </div>
 
                   <header className='flex justify-between'>
@@ -218,6 +418,7 @@ export function CentrosPage() {
                                  className='my-2 flex items-center gap-3'
                               >
                                  <Input
+                                    value={value.nombre ?? ''}
                                     className='w-full p-1'
                                     placeholder='Nombre y apellidos'
                                     name={`tutor_${index}`}
@@ -378,7 +579,6 @@ export function CentrosPage() {
                         id='telefono'
                         className='w-full p-2'
                         placeholder='XXX XX XX XX'
-                        required
                      />
                   </div>
 
@@ -395,7 +595,6 @@ export function CentrosPage() {
                         id='movil'
                         className='w-full p-2'
                         placeholder='XXX XX XX XX'
-                        required
                      />
                   </div>
                </div>
@@ -422,6 +621,7 @@ export function CentrosPage() {
                   onSubmit={handleSubmit}
                >
                   <SearchSelect
+                     placeholder='Buscar'
                      filters={filters}
                      handleSelectChange={handleSelectChange}
                   />
