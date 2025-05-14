0b6753ac8a8ad9c32916824634ab044205f54631
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-05-14 19:25:36 +0200

    ReAdd Farmacia Model
    Go on #43

--- src/components/models/Farmacia.jsx
+++ src/components/models/Farmacia.jsx
@@ -1,3 +1,17 @@
+import { request } from '../../services'
 import { Centro } from './Centro.jsx'
 
-export class Farmacia extends Centro {}
+export class Farmacia extends Centro {
+   static getCentros(page, perPage, search, filter) {
+      return request.farmacias(page, perPage, search, filter)
+   }
+   static addCentro(centro) {
+      return request.addFarmacia(centro)
+   }
+   static editCentro(centro) {
+      return request.editFarmacia(centro)
+   }
+   static deleteCentro(id) {
+      return request.deleteFarmacia(id)
+   }
+}
