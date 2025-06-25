ffb198d9b3170e45b89576fc1aff612387ec856f
JoseCarlos <jcarloslopez@correo.ugr.es>
2025-06-25 19:03:38 +0200

    Add query param only if value is not empty
    Go on #72 and closes #70

--- src/utils/queryUrl.js
+++ src/utils/queryUrl.js
@@ -1,7 +1,9 @@
 export function queryString(queryParams = {}) {
    let queryString = ''
    for (const [key, value] of Object.entries(queryParams)) {
-      queryString += `${key}=${value}&`
+      if (value) {
+         queryString += `${key}=${value}&`
+      }
    }
 
    queryString = queryString.slice(0, queryString.length - 1)
